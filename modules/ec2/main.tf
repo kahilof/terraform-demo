271f942749caf3107b6c1dd07b5b282d73f882dd
Omar <kahilo@amazon.com>
2022-05-30 13:06:33 +0100

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -17,10 +17,11 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ec2_instance.id}"
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 
   tags = {
     Name = "EC2 Demo Instance"
   }
 }
+
