e6e7652d2216add88eb21fa9cb8ec9ca0986b83f
Omar <kahilo@amazon.com>
2022-05-30 12:58:29 +0100

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,12 @@
 data "aws_caller_identity" "current" {}
 
-data "aws_ami" "ec2_instance" {
+data "aws_ami" "ubuntu" {
   most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
   filter {
     name   = "virtualization-type"
