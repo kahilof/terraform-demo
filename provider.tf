8645e397d9b417b4dde462198d10283494f60d45
Omar <kahilo@amazon.com>
2022-05-30 12:54:48 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-467406547633"
+    key    = "terraform-state-467406547633/terraform-demo/"
+    region = "eu-west-1"
+  }
+}
+
 provider "aws" {
   region = "${var.region}"
 }
