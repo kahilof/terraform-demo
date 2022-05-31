4b8f5301b879ce763d2b52826cb1a1a22b87ab98
Emmanuel <esaliu@amazon.com>
2022-05-31 14:09:36 +0100

    reverting change

--- provider.tf
+++ provider.tf
@@ -1,13 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-terraform {
   backend "s3" {
     bucket = "this-is-terraform-state"
     key    = "terraform-demo/terraform.tfstate"
