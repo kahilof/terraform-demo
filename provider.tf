8ece2f7860441c1d3bdfd4c618fb6cdb06e424bf
Emmanuel <esaliu@amazon.com>
2022-05-31 14:05:31 +0100

    added in required provider

--- provider.tf
+++ provider.tf
@@ -1,4 +1,13 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+terraform {
   backend "s3" {
     bucket = "this-is-terraform-state"
     key    = "terraform-demo/terraform.tfstate"
