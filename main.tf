dbb1c4cbcc9cad262604bca148b709a8ccb132cc
Emmanuel <96132245+Emmanuel-AWS@users.noreply.github.com>
2022-06-14 12:49:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-module "ec2" {
-  source = "./modules/ec2"
+# module "ec2" {
+#   source = "./modules/ec2"
 
-  region        = var.region
-  instance_name = var.instance_name
-}
+#   region        = var.region
+#   instance_name = var.instance_name
+# }
 
 module "s3" {
   source = "./modules/s3"
